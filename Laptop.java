16f4c6049449ef0ae8e543c1d41063a3bd14d484
Agnieszka Blicharz <bli210942@spengergasse.at>
2026-09-23 21:21:19 +0200

    setER

--- Laptop.java
+++ Laptop.java
@@ -3,4 +3,20 @@ public class Laptop
     private String  hersteller;
     private int     kosten;
     private boolean neu;
+    
+   public void setHersteller(String neuHersteller)
+   {
+        hersteller = neuHersteller;
+        
+   }
+   
+   public void setKosten(int neuKosten)
+   {
+        kosten = neuKosten;
+   }
+    
+   public void setNeu(boolean neuNeu)
+   {
+       neu = neuNeu;
+   }
 }
